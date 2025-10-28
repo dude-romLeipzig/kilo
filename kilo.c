d1dc436f1f2491023a116d0ee2f7e3b74b283571
Chris Iiams <mpishimkubwa@gmail.com>
2025-10-28 20:31:47 +0100

    updates, you now get an output of the asci value and its corresponding symbol of the key pressed. AND, the read is Bytewise instead of per line.

--- kilo.c
+++ kilo.c
@@ -1,21 +1,37 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-	struct termios raw;
-
-	tcgetattr(STDIN_FILENO, &raw);
-
-	raw.c_lflag &= ~(ECHO);
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+	
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-	enableRawMode():
+	enableRawMode();
 	
 	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else{
+			printf("%d ('%c')\n", c, c);
+		}
+	}
+
 	return 0;
 }
 
